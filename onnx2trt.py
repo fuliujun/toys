c87c327924727bce5061967444e1d9456b6d53bc
fuliujun <1104285822@qq.com>
2021-07-22 09:17:23 +0000

    version 4

--- onnx2trt.py
+++ onnx2trt.py
@@ -3,7 +3,7 @@ import sys
 def build_engine(onnx_file_path,engine_save_path):
     TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
     EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
-    with trt.Builder(TRT_LOGGER) as builder, builder.create_network() as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
+    with trt.Builder(TRT_LOGGER) as builder, builder.create_network(EXPLICIT_BATCH) as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
         with builder.create_builder_config() as config:
             config.max_workspace_size = 1 << 20
             config.max_batch_size = 1
