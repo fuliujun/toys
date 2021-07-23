932fdea38d60606549db27d112d3605730dcb71e
fuliujun <1104285822@qq.com>
2021-07-23 03:20:37 +0000

    test v1

--- onnx2trt.py
+++ onnx2trt.py
@@ -7,17 +7,16 @@ def build_engine(onnx_file_path,engine_save_path):
         with builder.create_builder_config() as config:
             config.max_workspace_size = 1 << 31
             profile = builder.create_optimization_profile()
-            profile.set_shape("foo", (1,3,244,244),(4,3,244,244),(8,3,244,244))
+            profile.set_shape("foo", (1,3,244,244),(4,3,244,244),(8,3,2446,244))
             config.add_optimization_profile(profile)
-            #config.max_batch_size = 1
-        with open(onnx_file_path,'rb') as model:
-            if not parser.parse(model.read()):
-                for error in range(parser.num_errors):
-                    print(parser.get_error(error))
-            else:
-                engine = builder.build_engine(network, config)
-                with open(engine_save_path, 'wb') as f:
-                    f.write(engine.serialize())
+            with open(onnx_file_path,'rb') as model:
+                if not parser.parse(model.read()):
+                    for error in range(parser.num_errors):
+                        print(parser.get_error(error))
+                else:
+                    engine = builder.build_engine(network, config)
+                    with open(engine_save_path, 'wb') as f:
+                        f.write(engine.serialize())
                 
 
 if __name__ == "__main__":
